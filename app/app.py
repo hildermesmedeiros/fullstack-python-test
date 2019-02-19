0dee028f91c9c0bff92adfbff4b2891fda1f6c67
hildermesmedeiros <hildermes@gmail.com>
2019-02-19 17:09:55 -0300

    first flask form username and password auth (ps Without hash)

--- app/app.py
+++ app/app.py
@@ -1,12 +1,20 @@
 from typing import List, Dict
-from flask import Flask
+from flask import Flask, request, render_template, json
 import mysql.connector
-import json
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, InputRequired
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'DoNoEVERshareittonoone'
 
+class signinForm(FlaskForm):
+	#flask recommendation is to use inputRequired instead of datarequired
+    username = StringField('Nome do usuário', validators = [DataRequired()])
+    password = password = PasswordField('password', validators=[InputRequired()])
 
-def user_info() -> List[Dict]:
+def user_login() -> List[Dict]:
 #database user, pswd, infos, and connection
     config = {
         'user': 'root',
@@ -19,19 +27,33 @@ def user_info() -> List[Dict]:
 #python postgresql connection
     cursor = connection.cursor()
 #Selectin first table
-    cursor.execute('SELECT * FROM siteuser')
+    user= str('hildermes')
+    cursor.execute('SELECT uname FROM tm_siteuser WHERE uname=%s',[user])
 #Showing something
-    results = [{uname: email} for (uname, email) in cursor]
+    data=cursor.fetchone()
+    results = data
 #closing the connection need to do something
     cursor.close()
 #closing the db connection
     connection.close()
     return results
 
-
+'''
 @app.route('/')
 def index() -> str:
-    return json.dumps({'siteuser': user_info()})
+    return json.dumps({'siteuser': user_login()})
+'''
+@app.route('/')
+def index():
+    form = signinForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('index.html', form=form)
+
+@app.route('/signin', methods=['GET','POST'])
+def signin_page():
+    form = signinForm
+
 
 ##Coonection to external port, binding to
 if __name__ == '__main__':
