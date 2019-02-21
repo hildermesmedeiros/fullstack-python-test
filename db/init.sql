49b845e53b438685f3227ae60392a024444123f7
hildermesmedeiros <hildermes@gmail.com>
2019-02-21 17:15:29 -0300

    droping one table a inserting cascade and delete on update

--- db/init.sql
+++ db/init.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE telemedicina;
 use telemedicina;
 SET sql_mode = '';
+
 DROP TABLE IF EXISTS tm_siteuser;
 CREATE TABLE tm_siteuser (
   id    INTEGER NOT NULL 
@@ -17,15 +18,6 @@ CREATE TABLE tm_siteuser (
   typeid INT(2) NOT NULL
 )ENGINE=INNODB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS tm_client;
-CREATE TABLE tm_client(
-  id INTEGER NOT NULL PRIMARY KEY,
-  user_id INTEGER,
-  INDEX u_ind (user_id),
-  FOREIGN KEY (user_id)
-    REFERENCES tm_siteuser(id)
-)ENGINE=INNODB DEFAULT CHARSET=utf8;
-
 DROP TABLE IF EXISTS tm_products;
 CREATE TABLE tm_products (
   id INTEGER NOT NULL 
@@ -38,7 +30,9 @@ CREATE TABLE tm_products (
   user_id INTEGER,
   INDEX u_id (user_id),
   FOREIGN KEY (user_id)
-    REFERENCES tm_client(user_id)
+    REFERENCES tm_siteuser(id)
+   ON DELETE CASCADE
+   ON UPDATE CASCADE 
 )ENGINE=INNODBD DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS tm_order;
@@ -47,20 +41,10 @@ CREATE TABLE tm_order(
   product_id INT NOT NULL,
   client_id INTEGER NOT NULL REFERENCES tm_user(id),
   INDEX (product_id),
-  INDEX (client_id),
-  
   FOREIGN KEY (product_id)
     REFERENCES tm_products(id)
     ON UPDATE CASCADE ON DELETE RESTRICT,
-  
+  INDEX (client_id), 
   FOREIGN KEY (client_id)
-    REFERENCES tm_client(id)
-  
+    REFERENCES tm_siteuser(id)  
 )ENGINE=INNODB DEFAULT CHARSET=utf8;
-
-INSERT INTO tm_siteuser(
-  uname, fname, mname, email, niver, hashpass,typeid
-)
-VALUES
-  ('test', 'teste', 'teste', 'teste','1987-12-04', 'cf23df2207d99a74fbe169e3eba035e633b65d94', '2');
-
