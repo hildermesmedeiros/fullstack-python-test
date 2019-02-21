3795787629cc86407f0e4389831edccfc79b5ab0
hildermesmedeiros <hildermes@gmail.com>
2019-02-21 14:20:48 -0300

    back end tests

---
+++ app/testes.py
@@ -0,0 +1,8 @@
+from backEnd import Users
+import time  
+#import datetime
+
+Users.gettingUsers()
+Users.add_user('hildermes','Hildermes José','Medeiros Filho', 'hildermes@gmail.com','1987-12-04', 'tele', '2')
+Users.add_user('Carlos','Hernesto','Medeiros', 'carlos@gmail.com','1948-07-05', 'carlgomes', '1')
+Users.gettingUsers()
