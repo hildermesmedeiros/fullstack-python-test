238812e15f8e16ba7589aa21de9746db8876dd69
hildermesmedeiros <hildermes@gmail.com>
2019-02-21 21:33:28 -0300

    market functions updates

--- app/backEnd/market.py
+++ app/backEnd/market.py
@@ -78,7 +78,7 @@ def delete_product_by_id(product_id):
     except Exception as e:
         print("Error ",e, " ao deletar o produto de id:[",product_id,"]")
     
-def show_produtct_info(product_id):
+def show_product_info(product_id):
     try:
         print("Informações sobre o produto")
         config = {
@@ -103,3 +103,49 @@ def show_produtct_info(product_id):
             print(data)
     except Exception as e:
         print("Error", e, " ao buscar pelo produto de id:[", product_id,"]")
+        
+def update_product(product_id, Dict):
+    try:
+        print("Atualizando informações do produto")
+    
+        config = {
+        'user': 'root',
+        'password': 'root',
+        'host': 'db',
+        'port': '3306',
+        'database': 'telemedicina'
+        }
+        
+        connection = mysql.connector.connect(**config)	
+        cursor = connection.cursor()
+        sql = 'SELECT name, description, price FROM tm_products WHERE id = %s'
+        adr = (product_id,)
+        cursor.execute(sql, adr)
+        product = []
+        for row in cursor:
+            a = list(row)
+            product= {'Product Name': a[0], 'Product Description': a[1], 'Product Price': a[2]}
+         
+        for key in list(Dict.keys()):
+            if key == 'Product Name':
+                product[key] = Dict[key]
+            elif key == 'Product Description':
+                product[key] = Dict[key]
+            elif key == 'Product Price':
+                product[key] = Dict[key]
+            else:  
+                Print('Dictionaries keys are not equal')
+                Print("ie: {'Product Name': 'carro' , 'Product Description': 'ambulância', 'Product Price': 'R$ 100.000,00}'")
+                return
+                
+        cursor.close()
+        cursor = connection.cursor()
+        sql = 'UPDATE tm_products SET name = %s, description = %s, price = %s WHERE id = %s'
+        adr = (product['Product Name'],product['Product Description'], product['Product Price'],product_id,)
+        cursor.execute(sql, adr)
+        cursor.close()
+        connection.commit()
+        connection.close()
+        
+    except Exception as e:
+        print(' ALGUM ERRO ACONTECEU com update product. Erro: ', e)
