5fc5a5ea718daa2ef145d28cf0b14e10c1ea86df
hildermesmedeiros <hildermes@gmail.com>
2019-02-28 15:08:59 -0300

    adding user true command line, checking if does it right
    ps: to verbose, must change it

--- app/testes.py
+++ app/testes.py
@@ -1,38 +1,24 @@
+# -*- coding: utf-8 -*-
 from backEnd import Users
 from backEnd.models import tm_siteuser
 from backEnd import products
 import time
 from backEnd import app, db  
+from backEnd.models import tm_siteuser
+from backEnd import bcrypt
 #import datetime
 
-from werkzeug.security import generate_password_hash, check_password_hash
-
-
-User = tm_siteuser(
-    username = 'hildermes',
-    firstname = 'Hildermes José',
-    middlename = 'Medeiros',
-    email = 'hildermes@gmail.com',
-    birthday = '1987-12-04',
-    t1 = time.strftime('%Y-%m-%d %H-%M-%S'),
-    hashpass = generate_password_hash('tele'),
-    typeid = 2
-)
-db.session.add(User)
-try:
-    db.session.commit()
-except:
-    db.session.rollback()    
-username='hildermes'
-userslist = tm_siteuser.query.filter_by(username=username).first()
-print('users:', userslist.username)
+Users.add_user('hildermes', 'Hildermes José', 'Medeiros', 'hildermes@gmail.com', '1987-12-04','tele', 1)
+hildermes=tm_siteuser.query.filter_by(username='hildermes').first()
+print(hildermes.username)
+print(hildermes.hashpass)
+password=bcrypt.generate_password_hash(u'hildermes').decode('utf-8')
+print(password)
+a=bcrypt.check_password_hash(hildermes.hashpass,'tele')
+print(a)
+#Users.add_user('Carlos','Hernesto','Medeiros', 'carlos@gmail.com','1948-07-05', 'carlgomes', 1)
+Users.add_user('teste1','Hernesto','Medeiros', 'teste1@gmail.com','1948-07-05', 'carlgomes', 1)
 '''
-Users.gettingUsers()
-#(username, firstname, middlename, email, birthday, password, usertype)
-Users.add_user('hildermes','Hildermes José','Medeiros Filho', 'hildermes@gmail.com','1987-12-04', 'tele', '2')
-Users.add_user('Carlos','Hernesto','Medeiros', 'carlos@gmail.com','1948-07-05', 'carlgomes', '1')
-Users.gettingUsers()
-
 #(username, product_name, product_description, product_price):
 products.insert_product('hildermes', 'luvas médicas', '100 luvas de latex descartáveis', 'R$ 29,90')
 products.show_product_info('1')
@@ -46,3 +32,10 @@ products.show_product_info('2')
 products.update_product('1', {'Product Name': 'carro' , 'Product Description': 'ambulância', 'Product Price': 'R$ 100.000,00'})
 products.show_product_info('1')
 '''
+print(" ___             ___       _____   ___     ___  _____    ____")
+print("|     |\\    |  |    \\        |    |       /       |     /  ")
+print("|     | \\   |  |     \\       |    |       |       |     |  ")
+print("|---  |  \\  |  |     |       |    |---     \\      |      \\ ")
+print("|     |   \\ |  |     /       |    |        /      |      /  ")
+print("|___  |    \\|  |____/        |    |___  __/       |   __/   ")
+print("                                                               ")
