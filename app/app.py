8778b07b5d7361f73cb3c91ed2850f64a1e5322f
hildermesmedeiros <hildermes@gmail.com>
2019-02-28 14:12:13 -0300

    many changes
    1 - changed from werkzeug to bcrypt
    2 - installed debuger
    3 - set env variables to debug so flask knows I'm in debug

--- app/app.py
+++ app/app.py
@@ -2,20 +2,28 @@
 #----------------------------------------------------------------------------#
 # Imports
 #----------------------------------------------------------------------------#
-from flask import (Flask, render_template, request, g, session, flash, redirect,
+from flask import (Flask, render_template, request, flash, redirect,
     url_for, abort)
-from functools import wraps
-import logging
-from logging import Formatter, FileHandler
+from flask_login import (current_user, login_user, login_required, logout_user)
 from forms import *
 import os
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import AnonymousUserMixin as notUser
+from flask_debugtoolbar import DebugToolbarExtension
+from werkzeug.urls import url_parse
+import logging
+#from werkzeug.security import generate_password_hash, check_password_hash
 #----------------------------------------------------------------------------#
-# Mybackends (testing)
+# My backends (testing)
 #----------------------------------------------------------------------------#
+#importing app and database (defined in init, this can cause 
+#circular imports)
 from backEnd import app, db
-#from backEnd import Users
-
+#I instanciated bycrypt in the init
+from backEnd import bcrypt 
+#Importing the database models
+from backEnd.models import tm_siteuser
+#importing loging instanciated in the init
+from backEnd import login_manager
 # Automatically tear down SQLAlchemy.
 '''
 @app.teardown_request
@@ -23,25 +31,21 @@ def shutdown_session(exception=None):
     db_session.remove()
 '''
 
-# Login required decorator.
-
-def login_required(test):
-    @wraps(test)
-    def wrap(*args, **kwargs):
-        if 'logged_in' in session:
-            return test(*args, **kwargs)
-        else:
-            flash('Você Precisa Logar Primeiro.')
-            return redirect(url_for('login'))
-    return wrap
-
+toolbar = DebugToolbarExtension(app)
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.config['DEBUG_TB_PROFILER_ENABLED'] = True
 #----------------------------------------------------------------------------#
 # Controllers.
 #----------------------------------------------------------------------------#
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'], endpoint='home')
 def home():
+    #If its anonymous user
+    if notUser.is_anonymous:
+        flash(u"Não esqueça de logar")
+    elif notUser.is_anonymous==True:
+        login_manager.login_message = u"Bem vindo, você acaba de logar."
     return render_template('pages/placeholder.home.html')
 
 
@@ -50,24 +54,23 @@ def about():
     return render_template('pages/placeholder.about.html')
 
 
-@app.route('/login', methods=['GET','POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    logging.warning("See this message in Flask Debug Toolbar!")
     form = LoginForm(request.form)
-    try:
-        if request.method == 'POST':
-            if form.validate_on_submit():
-                user = tm_siteuser.query.filter_by(username=request.form['name'])
-                if user != None and check_password_hash(user.password,request.form['name']):
-                    session['logged_in']=True
-                    flash('Você foi logado a página')
-                    return redirect(url_for('home'))
-                else:
-                    flash('Algum erro')
-                    render_template('forms/login.html', form=form)
-        return render_template('forms/login.html', form=form)      
-    except Exception as e:
-        flash('ALGUM ERRO ACONTECEU. Erro: ', e)
-        return render_template('forms/login.html', form=form)            
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            user = tm_siteuser.query.filter_by(username=request.form['username']).first()
+            logging.warning(str(user))
+            if user is not None and bcrypt.check_password_hash(
+                user.hashpass, request.form['password']
+            ):
+                login_user(user)
+                flash(u'You were logged in. Go Crazy.')
+                return redirect(url_for('home'))
+               
+    return render_template('forms/signin.html', form=form) 
+                     
 '''
 def login():
     error = None
@@ -85,9 +88,9 @@ def login():
     return render_template('forms/login.html', form=form)
 '''
 @app.route('/logout')
+@login_required
 def logout():
-    session.pop('logged_in', None)
-    flash('Você deslogu')
+    logout_user()
     return redirect(url_for('home'))
     
 @app.route('/register', methods=['GET','POST'])
@@ -101,13 +104,14 @@ def register():
                 firstname = form.firstname.data,
                 middlename = form.middlename.data,
                 birthday = form.birthday.data,
-                hashpass = generate_password_hash(form.password.data),
+                #models converts pass to hash
+                hashpass = form.password.data,
                 timestamp = time.strftime('%Y-%m-%d %H-%M-%S'),
                 typeid = 1
             )
             db.session.add(tm_siteuser)
             db.session.commit()
-            return redirect(url_for('home'))
+            return redirect(url_for('index'))
     return render_template('forms/register.html', form=form)
 
 
@@ -128,15 +132,6 @@ def internal_error(error):
 def not_found_error(error):
     return render_template('errors/404.html'), 404
 '''
-if not app.debug:
-    file_handler = FileHandler('error.log')
-    file_handler.setFormatter(
-        Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
-    )
-    app.logger.setLevel(logging.INFO)
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
-    app.logger.info('errors')
 
 #----------------------------------------------------------------------------#
 # Launch.
@@ -144,4 +139,4 @@ if not app.debug:
 
 # start the server with the 'run()' method
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0')
