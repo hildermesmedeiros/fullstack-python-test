5d9b3a88749b368ddf51d00827b319d29ad6bcae
hildermesmedeiros <hildermes@gmail.com>
2019-03-06 08:16:41 -0300

    the date field wouldn't validate, now it does.
    also changed the confirm, but I should add some validation in this file.

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from flask_wtf import FlaskForm
 from wtforms import TextField, PasswordField
+#for some reason 
 from wtforms.fields.html5 import DateField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, InputRequired, Length
 from backEnd import tm_siteuser
@@ -18,30 +19,27 @@ class RegisterForm(FlaskForm):
         'Sobre nome', validators=[DataRequired(), Length(min=6, max=40)]
     )
     birthday = DateField(
-        'Data de Nascimento',format='%d-%m-%Y', validators=[DataRequired()]
+        'Data de Nascimento',format="%Y-%m-%d", validators=[DataRequired()]
     )        
     email = TextField(
         'Email', validators=[DataRequired(), Length(min=6, max=40)]
     )
     password = PasswordField(
-        'Senha', validators=[DataRequired(), Length(min=6, max=40)]
+        'Senha', validators=[DataRequired(), Length(min=6, max=40), EqualTo('confirm', message='As senhas devem ser iguais')]
     )
     confirm = PasswordField(
-        'Repita a Senha',
-        [DataRequired(),
-        EqualTo('Senha', message='As senhas devem ser iguais')]
+        'Repita a Senha'
     )
     
     def validate_name(self, name):
-        user = tm_siteuser.query.filter_by(username=name.data).first()
-        if user != None:
+        name = tm_siteuser.query.filter_by(username=name.data).first()
+        if name != None:
             raise ValidationError('Este nome de usuário não está disponível')
 
     def validate_email(self, email):
-        user = tm_siteuser.query.filter_by(email=email.data).first()
-        if user != None:
+        email = tm_siteuser.query.filter_by(email=email.data).first()
+        if email != None:
             raise ValidationError('Este email já está cadastrado')
-
 
 class LoginForm(FlaskForm):
     username = TextField('Nome', [DataRequired()])
