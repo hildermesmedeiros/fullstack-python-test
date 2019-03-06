829733cee50acf9ddf88afc5b54a630524fb4fdc
hildermesmedeiros <hildermes@gmail.com>
2019-03-06 08:05:13 -0300

    many changes
    1- added a logo button, if usuers are logged, they should see only
    logout.
    2- added 2 routes, a home and about for logged users.
    3- now users can register, but I might change the forms.py,
    I'd like to se some warnings in some cases

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@
 # Imports
 #----------------------------------------------------------------------------#
 from flask import (Flask, render_template, request, flash, redirect,
-    url_for, abort)
+    url_for, abort, g, session)
 from flask_login import (current_user, login_user, login_required, logout_user, confirm_login)
 from forms import *
 import os
@@ -22,40 +22,47 @@ from backEnd import app, db
 from backEnd import bcrypt 
 #Importing the database models
 from backEnd.models import tm_siteuser
+from backEnd import Users
 #importing loging instanciated in the init
 from backEnd import login_manager
 # Automatically tear down SQLAlchemy.
-'''
-@app.teardown_request
-def shutdown_session(exception=None):
-    db_session.remove()
-'''
+
+
+
 
 toolbar = DebugToolbarExtension(app)
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['DEBUG_TB_PROFILER_ENABLED'] = True
+@app.teardown_request
+def shutdown_session(exception=None):
+    db.session.remove()
 #----------------------------------------------------------------------------#
 # Controllers.
 #----------------------------------------------------------------------------#
-
-
 @app.route('/', methods=['GET', 'POST'], endpoint='home')
-def home():
-    if current_user.is_anonymous:
-        usermsg= 'Não esqueça de logar'
-    elif current_user.is_authenticated:
-        usermsg = str('Olá ', current_user.username)
-    return render_template('pages/placeholder.home.html', usermsg=usermsg)
+def home(): 
+    if current_user.is_authenticated:
+        user_id=session['user_id']
+        return redirect(url_for('homewithuser', user_id=user_id))
+    return render_template('pages/placeholder.home.html')
 
-
-@app.route('/about')
+@app.route('/user/<user_id>', methods=['GET','POST'], endpoint = 'homewithuser')
+def homewithuser(user_id):
+    user_id=session['user_id']
+    return render_template('pages/placeholder.homewithuser.html')
+    
+@app.route('/about', methods=['GET', 'POST'], endpoint='about')
 def about():
     return render_template('pages/placeholder.about.html')
 
+@app.route('/user/about/<user_id>', methods=['GET', 'POST'], endpoint='aboutuser')
+def about(user_id):
+    user_id=session['user_id']
+    return render_template('pages/placeholder.aboutuser.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if notUser.is_anonymous:
+    if current_user.is_authenticated == False:
         logging.warning("Signin post ")
         form = LoginForm(request.form)
         if request.method == 'POST':
@@ -66,10 +73,13 @@ def login():
                     user.hashpass, request.form['password']
                 ):
                     login_user(user)
-                    return redirect(url_for('home'))
+                    user_id =session['user_id']
+                    return redirect(url_for('homewithuser', user_id=user_id))
                 
         return render_template('forms/signin.html', form=form) 
-    return redirect(url_for('home'))    
+    elif current_user.is_authenticated:
+        user_id=session['user_id']
+        return redirect(url_for('homewithuser', user_id=user_id))    
              
 @app.route('/logout')
 @login_required
@@ -77,40 +87,40 @@ def logout():
     if current_user.is_authenticated:
         logout_user()
         return redirect(url_for('home'))
-    return redirect(url_for('home'))
     
 @app.route('/register', methods=['GET','POST'])
 def register():
     if current_user.is_authenticated:
-        return redirect(url_for('home'))
+        user = tm_siteuser.query.filter_by(username=session["user_id"]).first()
+        user_id = session['user_id']
+        return redirect(url_for('homewithuser', user_id=user_id))
         
-    logout_user()     
-    form = RegisterForm(request.form)
-    error = None
-    if request.method == 'POST':
-        if form.validate_on_submit():
-            tm_siteuser(
-                username = form.username.data,
-                email = form.email.data,
+    elif current_user.is_authenticated == False:  
+        form = RegisterForm(request.form)
+        commiterror = None
+        otherError = None
+        logging.warning('User must fill form send true post')
+        
+        if request.method == "POST" and form.validate():
+            logging.warning('Form validated, trying to submit')
+            Users.add_user(
+                username = form.name.data,
                 firstname = form.firstname.data,
                 middlename = form.middlename.data,
+                email = form.email.data,
                 birthday = form.birthday.data,
                 #models converts pass to hash
-                hashpass = form.password.data,
-                timestamp = time.strftime('%Y-%m-%d %H-%M-%S'),
-                typeid = 1 
+                password = form.password.data,
+                usertype = 1             
             )
-
-            try:
-                db.session.commit()
-            except mysql.connector.Error as e1:
-                db.session.rollback()
-                error = e1    
-            finally:
-                if error == None:
-                    login_user(user)
-                    return redirect(url_for('home'))
-    return render_template('forms/register.html', form=form)
+            
+            if commiterror == None and otherError== None:
+                return redirect(url_for('login'))
+            else:        
+                pass
+        #flash(form.errors)
+        #flash(form.birthday.data)
+        return render_template('forms/register.html', form=form)
 
 
 @app.route('/forgot')
@@ -118,7 +128,7 @@ def forgot():
     form = ForgotForm(request.form)
     return render_template('forms/forgot.html', form=form)
 
-'''
+
 # Error handlers.
 @app.errorhandler(500)
 def internal_error(error):
@@ -129,7 +139,7 @@ def internal_error(error):
 @app.errorhandler(404)
 def not_found_error(error):
     return render_template('errors/404.html'), 404
-'''
+
 
 #----------------------------------------------------------------------------#
 # Launch.
